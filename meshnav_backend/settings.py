bc9c419741dba8a57190c75e97925bd731ca1615
rachitkumar205 <rk_programmer@outlook.com>
2024-02-03 23:43:16 +0530

    added cors origins

--- meshnav_backend/settings.py
+++ meshnav_backend/settings.py
@@ -32,6 +32,11 @@ ALLOWED_HOSTS = ['meshnav.azurewebsites.net']
 
 CSRF_TRUSTED_ORIGINS = ["https://meshnav.azurewebsites.net",'http://127.0.0.1']
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
