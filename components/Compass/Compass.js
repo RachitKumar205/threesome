984d39625b3ee726ce90f416a0870e20a058a527
Lalit M <lalitm1004@gmail.com>
2024-02-03 23:11:09 +0530

    add ()

--- components/Compass/Compass.js
+++ components/Compass/Compass.js
@@ -11,7 +11,7 @@ export default function Compass({northReset, waypointHeading, testOffset}) {
       <p>waypointHeading - {waypointHeading}</p>
       <p>testOffset - {testOffset}</p>
       <p>1+2 - {(waypointHeading + testOffset) - 360 * (Math.floor((waypointHeading + testOffset)/360))}</p>
-      <p>Resultant - {northReset + (waypointHeading + testOffset) - 360 * (Math.floor((waypointHeading + testOffset)/360))}</p>
+      <p>Resultant - {northReset + ((waypointHeading + testOffset) - 360 * (Math.floor((waypointHeading + testOffset)/360)))}</p>
       <hr/>
     </div>   
   )
