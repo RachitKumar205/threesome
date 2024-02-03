98e956834108561be5651518dd648538978adb63
Lalit M <lalitm1004@gmail.com>
2024-02-03 18:43:13 +0530

    Clean Code

--- app/page.js
+++ app/page.js
@@ -1,32 +1,30 @@
 "use client";
-
 import { useState, useEffect } from "react";
 import { useDeviceOrientation } from "./deviceOrientationHook";
 import Image from "next/image";
 
-import axios from "axios";
-import {getDistance, getPreciseDistance, isPointWithinRadius} from "geolib";
-
 import SelectDestination from "@/components/SelectDestination/SelectDestination";
+
+import {getDistance, getPreciseDistance, isPointWithinRadius} from "geolib";
 
 export default function Home() {
   const [destination, setDestination] = useState(null);
-  const [isToggled, setIsToggled] = useState(false);
-  const [orienation, requestAccess, revokeAccess, orienationError] = useDeviceOrientation();
   const [path, setPath] = useState(null);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [error, setError] = useState(null);
   const [distance, setDistance] = useState(null);
-  const [destinationReached, setDestinationReached] = useState(false);
+  const [nextWaypoint, setNextWaypoint] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const changeDestination = (destination) => {
-    console.log(destination)
-    console.log(latitude)
-    console.log(longitude)
     setDestination(destination);
+    setIsLoading(true);
     (async () => {
       try {
+        console.log(destination);
+        console.log(latitude);
+        console.log(longitude);
         const response = await fetch(
           "https://snunav.azurewebsites.net/api/path-api/",
           {
@@ -38,85 +36,76 @@ export default function Home() {
               target: destination.name,
               type: 'coordinates',
             }),
-          }
+          },
         );
 
         if (!response.ok) {
           throw new Error("API request failed");
         }
 
-        const data = await response.json();
-        console.log(data);
+        const data = await response.json()
         setPath(data);
-        
+        setNextWaypoint(data[0]);
       } catch (error) {
         setError(error);
+      } finally {
+        setIsLoading(false);
       }
-
     })();
   }
 
   useEffect(() => {
     if (navigator.geolocation) {
-      const watchId = navigator.geolocation.watchPosition(
-          (position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
+      const watchId = navigator.geolocation.watchPosition((position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
 
-            // Calculate distance only if both latitude, longitude, and responseData[0] are available
-            if (latitude && longitude && path && path[0]) {
-              setDistance(getDistance(
-                  { latitude: latitude, longitude: longitude },
-                  {
-                    latitude: path[0].latitude,
-                    longitude: path[0].longitude,
-                  }
-              ));	
-              if (isPointWithinRadius(
-                  { latitude: latitude, longitude: longitude },
-                  {
-                    latitude: path[0].latitude,
-                    longitude: path[0].longitude,
-                  },
-                  5
-              )) {
-                // Remove the first element from responseData
-                setPath(path.slice(1));
-
-                // Update currentWaypoint if more waypoints exist
-                if (path.length > 0) {
-                  setDestinationReached(false);
-                } else {
-                  setDestinationReached(true);
-                }
-              }
+        if (latitude && longitude && path) {
+          setDistance(getDistance(
+            { latitude: latitude, longitude: longitude },
+            {
+              latitude: nextWaypoint.latitude,
+              longitude: nextWaypoint.longitude,
             }
-          },
+          ));
 
-          (error) => alert(JSON.stringify(error)),
-          { enableHighAccuracy: true, distanceFilter: 1 }
+          if (distance <= 5) {
+            setPath(path.slice(1));
+            if (path.length > 0) {
+              setNextWaypoint(path[0]);
+            }
+          }
+
+        }
+      },
+        (error) => alert(JSON.stringify(error)),
+        { enableHighAccuracy: true, distanceFilter: 1 }
       );
 
+      // Cleanup code
       return () => {
         navigator.geolocation.clearWatch(watchId);
       };
     } else {
-      console.error('Geolocation API is not supported in this browser.');
-      setError('Geolocation is not supported in your browser.');
+      const error = new Error("Geolocation API is not supported in this browser");
+      setError(error);
     }
   }, [latitude, longitude, path]);
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
+    <div className="app min-h-screen flex flex-col justify-center items-center">
       <SelectDestination
         setDestination={changeDestination}
       />
-
-      <div className="compass">
-
-      </div>
-
-
+      {!isLoading && !error && (
+        <>
+          <div>
+            {path.map((item) => (
+              <p>{item.latitude}</p>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   )
 
