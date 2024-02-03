df6328a06e61c8f6407832f4cfec6587861464d1
Lalit M <lalitm1004@gmail.com>
2024-02-03 19:26:12 +0530

    Display next heading

--- app/page.js
+++ app/page.js
@@ -133,6 +133,7 @@ export default function Home() {
       {!isLoading && !error && (
         <>
           <div>
+            Angle - {nextWaypointHeading}
             {path.map((item) => (
               <p>{item.latitude}</p>
             ))}
